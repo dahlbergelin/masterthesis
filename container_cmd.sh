078050ecbae63c8765116fa9365a91d4377ee847
Elin Dahlberg <elindahlberg97@hotmail.com>
2024-11-06 14:46:06 +0100

    latest cchanges

--- container_cmd.sh
+++ container_cmd.sh
@@ -13,11 +13,10 @@ sudo docker run -v ~/project:/gatk/project \
 
 /gatk/project/sample_id_table/sample_id_table.txt
 # to update version outside my container
-scp /Users/qluelda/Desktop/Master_thesis/run_pipeline.sh qluelda@bender:~/project/my_scripts
-scp /Users/qluelda/Desktop/Master_thesis/final_mutect2.sh qluelda@bender:~/project/my_scripts
-scp /Users/qluelda/Desktop/Master_thesis/final_mutect2.sh qluelda@bender:~/project/my_scripts
-scp /Users/qluelda/Desktop/Master_thesis/final_vardict.sh qluelda@bender:~/project/my_scripts
-scp /Users/qluelda/Desktop/elin_bin.sh qluelda@bender:~/project/my_scripts
+scp /Users/qluelda/Desktop/collectSM.sh qluelda@bender:~/project/my_scripts/
+scp /Users/qluelda/Desktop/Master_thesis/final_mutect2.sh qluelda@bender:~/project/my_scripts/
+scp /Users/qluelda/Desktop/elin_bin.sh qluelda@bender:~/project/my_scripts/
+scp /Users/qluelda/Desktop/Master_thesis/run_pipeline.sh qluelda@bender:~/project/my_scripts/
 
 # run the script in the container 
 # to run on windows
@@ -39,11 +38,7 @@ chmod +x final_mutect2.sh
 chmod +x elin_bin.sh
 chmod +x collectSM.sh
 
-# <<<<<<<< VIEW RESULUTS >>>>>>>>>
-    grep -v '^#' filtered_without_seg_5158a03.vcf.gz | cut -f 1-7 | head -n 10
-    grep -v '^#' funcotated_output_5158a03.vcf | cut -f 1-8 | head -n 10
-    gzcat  HG38_f1r1.tar.gz | head
-    gzcat  somatic_variants.vcf.gz | less
+
 head -n 10 /gatk/data/elin_output/contamination.table # get the ten first heads in the file
 cat /gatk/project/run_pipeline.sh #view script
 zless /gatk/data//elin_output/filtered_without_seg.vcf.gz # view a xipped file. press key: q to exit 
@@ -67,16 +62,41 @@ screen -r session_name #3. Reattach to a Screen Session
 screen -ls #List All Active Screen Sessions
 screen -S session_name -X quit #kill a screen session
 
+# <<<<<<<<   WORKFLOW MUTECT2   >>>>>>>>>
+# Reference genome
+
 # Baserecalibration
 wget https://ftp.ncbi.nih.gov/snp/organisms/human_9606_b151_GRCh38p7/VCF/common_all_20180418.vcf.gz.tbi
 wget https://ftp.ncbi.nih.gov/snp/organisms/human_9606_b151_GRCh38p7/VCF/common_all_20180418.vcf.gz
+
 # index the .vcf file 
 gatk IndexFeatureFile -I /gatk/project/common_all_20180418.vcf # output: /gatk/project/common_all_20180418.vcf.idx
+
 # update the .vcf file with the right names 1 -> chr1
 awk '{if($1 ~ /^[0-9]+$/ || $1 ~ /^X$/ || $1 ~ /^Y$/) $1="chr"$1; print}' common_all_20180418.vcf | \
 bgzip -c > updated_common_all_20180418.vcf.gz
 tabix -p vcf /gatk/project/updated_common_all_20180418.vcf.gz # index the new file
 # now update the script with updated_common_all_20180418.vcf.gz
+
+# dowload a panel of normals 
+gsutil cp gs://gatk-best-practices/somatic-hg38/1000g_pon.hg38.vcf.gz /gatk/project/
+
+# index the .vcf file for PoN 
+gatk IndexFeatureFile \
+    -I /gatk/project/1000g_pon.hg38.vcf.gz
+
+# download the germline-resource file
+gsutil cp gs://gatk-best-practices/somatic-hg38/af-only-gnomad.hg38.vcf.gz /gatk/project/
+gsutil cp gs://gatk-best-practices/somatic-hg38/af-only-gnomad.hg38.vcf.gz.tbi /gatk/project/
+GERMLINE_RESOURCE="/gatk/project/af-only-gnomad.hg38.vcf.gz"
+
+# dowload common somatic variant sites .vcf
+gsutil cp gs://gatk-best-practices/somatic-hg38/small_exac_common_3.hg38.vcf.gz /gatk/project/
+
+# Get file name for segmentation in filter Mutec Calls 
+samtools view - H 
+# get the RG information from the .BAM file 
+samtools view -H Your_BAM_File.bam | grep '^@RG'
 
 # Get a .GTF file for the specific intervals
 curl -L -o gencode.v47.annotation.gtf.gz https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_47/gencode.v47.annotation.gtf.gz
@@ -88,58 +108,27 @@ java -jar picard.jar BedToIntervalList \
     O=hg38_exons.interval_list \
     SD=GRCh38.d1.vd1.dict
 
-gatk VariantsToTable \
-    -v "$FUNCOTATED_VCF" -F AC -F AN -F DP -F AF -F FUNCOTATION \
-    -O "/gatk/data/elin_output/${ID_PREFIX}/output-funct.table"
+# get funcoator version 
+gatk --java-options "-Xmx8g" FuncotatorDataSourceDownloader \
+    --somatic \
+    --validate-integrity \
+    -O /gatk/project/funcotator_dataSources.v1.7
 
-# <<<<<<<<   WORKFLOW MUTECT2   >>>>>>>>>
-# Reference genome - dowload file with curl
-curl -L -o GRCh38.d1.vd1_GATK_indices.tar.gz "https://api.gdc.cancer.gov/data/2c5730fb-0909-4e2a-8a7a-c9a7f8b2dad5"
-REF_GENOME="/gatk/project/GRCh38.d1.vd1.fa"
-REF_DICT="/gatk/project/GRCh38.d1.vd1.dict"
-REF_FAI="/gatk/project/GRCh38.d1.vd1.fa.fai"
-
-# Baserecalibration
-#wget https://ftp.ncbi.nih.gov/snp/organisms/human_9606_b151_GRCh38p7/VCF/common_all_20180418.vcf.gz.tbi
-#wget https://ftp.ncbi.nih.gov/snp/organisms/human_9606_b151_GRCh38p7/VCF/common_all_20180418.vcf.gz
-# Youtube - somatic pipeline with Mutect2 
-# download known sites files for BQSR from GATK resource bundle
-wget -P ~/gatk/project/ https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.dbsnp138.vcf
-wget -P ~/gatk/project/ https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.dbsnp138.vcf.idx
-
-# MUTECT2 Variant calling: dowload a panel of normals 
-gsutil cp gs://gatk-best-practices/somatic-hg38/1000g_pon.hg38.vcf.gz /gatk/project/
-PON_VCF="/gatk/project/1000g_pon.hg38.vcf.gz"  # Mutect2: Panel of Normals 1000 genomes
-
-# index the .vcf file for PoN 
-gatk IndexFeatureFile \
-    -I /gatk/project/1000g_pon.hg38.vcf.gz
-
-# download the germline-resource file
-gsutil cp gs://gatk-best-practices/somatic-hg38/af-only-gnomad.hg38.vcf.gz /gatk/project/
-gsutil cp gs://gatk-best-practices/somatic-hg38/af-only-gnomad.hg38.vcf.gz.tbi /gatk/project/
-GERMLINE_RESOURCE="/gatk/project/af-only-gnomad.hg38.vcf.gz"
-
-# GetPileUpSummeries: dowload common somatic variant sites .vcf
-gsutil cp gs://gatk-best-practices/somatic-hg38/small_exac_common_3.hg38.vcf.gz /gatk/project/
-CONT_VCF="/gatk/project/small_exac_common_3.hg38.vcf.gz" 
-
-# Get file name for segmentation in filter Mutec Calls 
-samtools view - H 
-# Get the RG information from the .BAM file 
-samtools view -H Your_BAM_File.bam | grep '^@RG'
-
-# FUNCOTATOR DATA SOURCES
-# get the latest version for hg:38
-FUNCOTATOR_DATA_SOURCES="/gatk/project/funcotator_dataSources.v1.8.hg38.20230908s"
-https://console.cloud.google.com/storage/browser/broad-public-datasets/funcotator/funcotator_dataSources.v1.8.hg38.20230908s?pageState=(%22StorageObjectListTable%22:(%22f%22:%22%255B%255D%22))
-# Unzip the file 
-sudo tar -xzvf funcotator_dataSources.v1.7.20200521s.tar.gz -C /gatk/project/
-# unzip the gnomad files in the directory 
-sudo tar -xzvf gnomAD_exome.tar.gz
-sudo tar -xzvf gnomAD_genome.tar.gz
-
-# view how many mutations that passed the filter
-view -f PASS filtered_without_seg_5158a03.vcf.gz | grep -v '^#' | wc -l
-bcftools view -f PASS filtered_without_seg_5158a03.vcf.gz | grep -v '^#' | head -n 10
-bcftools view -f PASS funcotated_output_5158a03.vcf | grep -v '^#' | head
+# view funcotated files
+bcftools view -f PASS filtered_without_seg_6f89146.vcf.gz | grep -v '^#' | wc -l
+bcftools view -f PASS funcotated_output_6f89146.vcf | grep -v '^#' | head -n 10
+# look at the output from the funcotation
+cat funcotated_output_6f89146.vcf | less
+cat output_6f89146.table | less
+# extract the infromation field from the funcotated file to the variant table 
+cat funcotated_output_6f89146.vcf | grep " Funcotation fields are: "
+# to build columns 
+cat funcotated_output_6f89146.vcf | grep " Funcotation fields are: " | sed 's/|/\t/g' | less
+# now save the file in your output folder
+cat funcotated_output_6f89146.vcf | grep " Funcotation fields are: " | sed 's/|/\t/g' > output_curated_variants_6f89146.txt
+# we can also just look at the gene we are interested in
+cat output_6f89146.table | cut -f 5 | grep "EGFR" | less
+# then insert a /tab instead of |
+cat output_6f89146.table | cut -f 5 | grep "EGFR" | sed 's/|/\t/g' | less
+# append this to the file we already created
+cat output_6f89146.table | cut -f 5 | grep "EGFR" || sed 's/|/\t/g' >> output_curated_variants_6f89146.txt
